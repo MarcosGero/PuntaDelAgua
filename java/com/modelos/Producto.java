c269256a2ff9b0e1dda1ab94d7d3010f1d8a8265
MarcosGero <marcos.funcky@gmail.com>
2023-09-10 01:26:34 -0300

    Modelos

---
+++ java/com/modelos/Producto.java
@@ -0,0 +1,19 @@
+package java.com.modelos;
+
+import java.util.List;
+
+
+
+public class Producto {
+    private String nombre;
+    private double precioMayor;
+    private double precioMinorista;
+    private int ID;
+    private String detalle;
+    private int cantidad;
+    private String lote;
+    private Proveedor proveedor;
+    private List<Venta> ventas;
+
+    // Constructores, getters, setters y otros métodos relevantes...
+}
