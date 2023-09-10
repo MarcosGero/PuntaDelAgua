4850fa4bc2dd0d1849f153a3df300edfe824fa72
MarcosGero <marcos.funcky@gmail.com>
2023-09-10 02:00:41 -0300

    Stock

--- java/com/modelos/Stock.java
+++ java/com/modelos/Stock.java
@@ -6,4 +6,10 @@ public class Stock {
     private String codigo;
     private String nombreProducto;
     private List<Producto> productos;
+
+    public Stock(String codigo, String nombreProducto, List<Producto> productos) {
+        this.codigo = codigo;
+        this.nombreProducto = nombreProducto;
+        this.productos = productos;
+    }
 }
