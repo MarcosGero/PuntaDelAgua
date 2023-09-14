0ce54dd69025fd63dca3e8a4965dec74a06539c0
MarcosGero <marcos.funcky@gmail.com>
2023-09-14 18:35:19 -0300

    spring

---
+++ src/main/java/com/pda/PdaApplication.java
@@ -0,0 +1,13 @@
+package com.pda.pda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdaApplication.class, args);
+	}
+
+}
