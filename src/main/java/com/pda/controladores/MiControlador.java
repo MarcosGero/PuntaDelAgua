87d826266bbd5ed4e415ddd100a34599a7feeb07
MarcosGero <marcos.funcky@gmail.com>
2023-09-19 09:49:42 -0300

    .

--- src/main/java/com/pda/controladores/MiControlador.java
+++ src/main/java/com/pda/controladores/MiControlador.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MiControlador {
-    @GetMapping("/")
+    @GetMapping("/hola")
     public String hola() {
         return "¡Hola Mundo!";
     }
