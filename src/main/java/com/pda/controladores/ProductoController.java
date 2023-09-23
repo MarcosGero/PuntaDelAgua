4841368a2d3548aa9d7668f9714316d6f18c875f
Octavio Riccardo <octariccardo@gmail.com>
2023-09-23 04:36:49 -0300

    Creando el primer servicio del controlador Producto

--- src/main/java/com/pda/controladores/ProductoController.java
+++ src/main/java/com/pda/controladores/ProductoController.java
@@ -1,28 +1,28 @@
 package com.pda.controladores;
 import com.pda.modelos.Producto;
+import com.pda.servicios.ProductoService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
-
-/* RestController importa los paquetes para indicar que la clase va a ser un controlador */
 @RestController
 
-/* RequestMapping define la ruta que se va a utilizar para obtener los datos en el navegador */
+
 @RequestMapping(path = "api/producto")
 
 public class ProductoController
 {
-    /* GetMapping se encarga de definir que la clase de va a comportar como una entidad GET en HTTP*/
-    @GetMapping
+    private final ProductoService productoService;
 
-    /* Se define un metodo para poder devolver la informacion del modelo en forma de JSON */
+    public ProductoController(ProductoService productoService){
+        this.productoService=productoService;
+    }
+
+    @GetMapping
     public Producto getProducto()
     {
-        Producto producto = new Producto();
-        return producto;
+         return this.productoService.getProducto();
     }
 
 }
