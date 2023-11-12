046ca0b70b9cb3b990e7010cfee5d191c1c0d687
MarcosGero <marcos.funcky@gmail.com>
2023-11-12 16:27:27 -0300

    .

--- src/main/java/com/pda/seeder/VentaSeeder.java
+++ src/main/java/com/pda/seeder/VentaSeeder.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 
 @Component
@@ -20,32 +21,37 @@ public class VentaSeeder implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        for (int i = 0; i < 20; i++) {
-            // Obtén productos aleatorios
-            Producto producto1 = productoDAO.findById((long) (Math.random() * 5 + 1)).orElseThrow();
-            Producto producto2 = productoDAO.findById((long) (Math.random() * 5 + 1)).orElseThrow();
+        if (ventaDAO.count() == 0) {
+            Calendar calendar = Calendar.getInstance();
+            for (int i = 0; i < 20; i++) {
+                calendar.add(Calendar.DAY_OF_YEAR, -i);
+                // Obtén productos aleatorios
+                Producto producto1 = productoDAO.findById((long) (Math.random() * 5 + 1)).orElseThrow();
+                Producto producto2 = productoDAO.findById((long) (Math.random() * 5 + 1)).orElseThrow();
 
-            // Cantidad y monto aleatorios (o fijos, según lo que necesites)
-            int cantidad1 = (int) (Math.random() * 10 + 1);
-            int cantidad2 = (int) (Math.random() * 10 + 1);
-            double monto1 = cantidad1 * producto1.getPrecioMinorista();
-            double monto2 = cantidad2 * producto2.getPrecioMinorista();
+                // Cantidad y monto aleatorios (o fijos, según lo que necesites)
+                int cantidad1 = (int) (Math.random() * 10 + 1);
+                int cantidad2 = (int) (Math.random() * 10 + 1);
+                double monto1 = cantidad1 * producto1.getPrecioMinorista();
+                double monto2 = cantidad2 * producto2.getPrecioMinorista();
 
-            Renglon renglon1 = new Renglon(producto1, cantidad1, monto1);
-            Renglon renglon2 = new Renglon(producto2, cantidad2, monto2);
+                Renglon renglon1 = new Renglon(producto1, cantidad1, monto1);
+                Renglon renglon2 = new Renglon(producto2, cantidad2, monto2);
 
-            // Total de la venta
-            double totalVenta = monto1 + monto2;
+                // Total de la venta
+                double totalVenta = monto1 + monto2;
 
-            // Crea una venta
-            Venta venta = new Venta(new Date(), totalVenta, Arrays.asList(renglon1, renglon2), TipoFactura.A, TipoVenta.MINORISTA);
+                // Crea una venta
+                Venta venta = new Venta(calendar.getTime(), totalVenta, Arrays.asList(renglon1, renglon2), TipoFactura.A, TipoVenta.MINORISTA);
 
-            // Establece la venta en cada renglón
-            renglon1.setVenta(venta);
-            renglon2.setVenta(venta);
+                // Establece la venta en cada renglón
+                renglon1.setVenta(venta);
+                renglon2.setVenta(venta);
 
-            // Guarda la venta
-            ventaDAO.save(venta);
+                // Guarda la venta
+                ventaDAO.save(venta);
+                calendar.setTime(new Date());
+            }
         }
 
     }
